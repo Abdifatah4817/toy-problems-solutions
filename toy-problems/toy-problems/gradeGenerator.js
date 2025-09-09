b4a679d384c6eee2a9612cedcd26582ef224c012
Abdifatah <mursalabdifatah17@gmail.com>
2025-09-09 11:37:07 +0300

    Complete all three challenges and update README

---
+++ toy-problems/toy-problems/gradeGenerator.js
@@ -0,0 +1,11 @@
+function gradeGenerator(marks) {
+    if (marks > 79) return "A";
+    else if (marks >= 60) return "B";
+    else if (marks >= 49) return "C";
+    else if (marks >= 40) return "D";
+    else return "E";
+}
+
+// Example
+const marks = 84;
+console.log("Grade:", gradeGenerator(marks));
